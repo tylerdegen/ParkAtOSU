9d10ccfe95199415419b2ce4edb5254c0117ed23
costa <costa.80@osu.edu>
2016-10-17 18:10:35 -0400

    Added loading, login, sign up and log out activities

--- app/src/main/java/com/parkatosu/parkatosu/signup.java
+++ app/src/main/java/com/parkatosu/parkatosu/signup.java
@@ -1,13 +1,73 @@
 package com.parkatosu.parkatosu;
 
+
+
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class signup extends AppCompatActivity {
+public class signup extends Fragment implements OnClickListener {
+    private EditText username;
+    private EditText password;
+    private EditText confirm;
+    private DatabaseHelper dh;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_signup, container, false);
+        username= (EditText)v.findViewById(R.id.username);
+        password= (EditText)v.findViewById(R.id.password);
+        confirm= (EditText)v.findViewById(R.id.password_confirm);
+        View btnAdd= (Button)v.findViewById(R.id.done_button);
+        btnAdd.setOnClickListener(this);
+        View btnCancel= (Button)v.findViewById(R.id.cancel_button);
+        btnCancel.setOnClickListener(this);
+        return v;
+    }
+
+    private void createAccount(){
+        String user = username.getText().toString();
+        String pass = password.getText().toString();
+        String conf = confirm.getText().toString();
+        if ((pass.equals(conf))&&(!user.equals(""))&&(!password.equals(""))){
+            this.dh = new DatabaseHelper(this.getActivity());
+            this.dh.insert(user, pass);
+            Toast.makeText(this.getActivity(),"new record inserted",Toast.LENGTH_SHORT).show();
+        }
+        else if(!pass.equals(conf)){
+            new AlertDialog.Builder(this.getActivity()).setTitle("Error").setMessage("Passwords do not match")
+                    .setNeutralButton("Try Again", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    }).show();
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.done_button:
+                createAccount();
+                break;
+            case R.id.cancel_button:
+                username.setText("");
+                password.setText("");
+                confirm.setText("");
+                break;
+        }
     }
 }
